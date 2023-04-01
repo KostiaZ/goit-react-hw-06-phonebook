b0a86cf8f14b7eeff7649201ef2c7a36a9b9c1e5
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-04-01 18:35:33 +0300

    rebuild phone book

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,23 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { filterContact } from 'redux/contactSlice';
+
+// import s from './Filter.module.css';
+
+const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.contacts.filter);
+
+  const filterChange = e => {
+    dispatch(filterContact(e.currentTarget.value));
+  };
+  return (
+    <div>
+      <label>
+        Filter
+        <input type="name" value={filter} onChange={filterChange} />
+      </label>
+    </div>
+  );
+};
+
+export default Filter;
